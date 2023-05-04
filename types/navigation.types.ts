702d110d12297d33696edb2c48e21597e1343fab
Osvaldo Costa <osvaldo.costajr@gmail.com>
2023-05-03 21:31:27 -0300

    Drawer interface & types name checked

--- types/navigation.types.ts
+++ types/navigation.types.ts
@@ -1,10 +1,10 @@
 import { CompositeNavigationProp } from "@react-navigation/native";
 import { NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
-import { DrawerStackParamList, RootStackParamList } from "App";
+import { DrawerParamList, RootStackParamList } from "App";
 
-export type CategoriesScreenNavigationProps = NativeStackScreenProps<DrawerStackParamList, 'MealsCategories'>;
-export type FavoriteScreenNavigationProps = NativeStackScreenProps<DrawerStackParamList, 'FavoriteScreen'>;
+export type CategoriesScreenNavigationProps = NativeStackScreenProps<DrawerParamList, 'MealsCategories'>;
+export type FavoriteScreenNavigationProps = NativeStackScreenProps<DrawerParamList, 'FavoriteScreen'>;
 export type MealsOverviewScreenNavigationProps = NativeStackScreenProps<RootStackParamList, 'MealsOverview'>;
 export type MealDetailScreenNavigationProps = NativeStackScreenProps<RootStackParamList, 'MealDetail'>;
 
